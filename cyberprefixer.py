800bf65236f4debf6d11a09d231cd1a32596a54b
Molly White <molly.white5@gmail.com>
2014-02-14 01:01:23 -0500

    Fix issues with the regex that broke the bot. Extra pipe, missing comma.

--- cyberprefixer.py
+++ cyberprefixer.py
@@ -42,7 +42,7 @@ offensive = re.compile(r"\b(deaths?|dead(ly)?|die(s|d)?|hurts?|"
                        r"tortur(e|ed|ing|es)|execut(e|ion|ed)s?|"
                        r"gun(man|men|ned)|suicid(e|al|es)|bomb(s|ed)?|"
                        r"mass[- ]?graves?|bloodshed|state[- ]?of[- ]?emergency|"
-                       r"al[- ]?Qaeda|blasts?|violen(t|ce))|lethal|\W?\b"
+                       r"al[- ]?Qaeda|blasts?|violen(t|ce))|lethal\W?\b",
                        flags=re.IGNORECASE)
 
 def get():
