be0cd1bfb474adc6a14808c117b634a8318e5cbe
Molly White <molly.white5@gmail.com>
2014-04-16 00:53:09 +0000

    Extend matches for bomb* in offensive.py

--- offensive.py
+++ offensive.py
@@ -5,7 +5,7 @@ offensive = re.compile(
     r"kill(ing|ed|er|s)?s?|wound(ing|ed|s)?|fatal(ly|ity)?|shoo?t(s|ing|er)?s?|crash(es|ed|ing)?|"
     r"attack(s|ers?|ing|ed)?|murder(s|er|ed|ing)?s?|hostages?|rap(e|es|ed|ing)|assault(s|ed)?|"
     r"pile-?ups?|massacre(s|d)?|assassinate(d|s)?|sla(y|in|yed|ys)|victims?|tortur(e|ed|ing|es)|"
-    r"execut(e|ion|ed)s?|gun(man|men|ned)|suicid(e|al|es)|bomb(s|ed)?|mass[- ]?graves?|bloodshed|"
-    r"state[- ]?of[- ]?emergency|al[- ]?Qaeda|blasts?|violen(t|ce)|lethal|cancer(ous)?|"
-    r"stab(bed|bing|ber)?|casualt(y|ies)|sla(y|ying|yer|in))\W?\b",
+    r"execut(e|ion|ed)s?|gun(man|men|ned)|suicid(e|al|es)|bomb(s|ed|ing|ings|er|ers)?|"
+    r"mass[- ]?graves?|bloodshed|state[- ]?of[- ]?emergency|al[- ]?Qaeda|blasts?|violen(t|ce)|"
+    r"lethal|cancer(ous)?|stab(bed|bing|ber)?|casualt(y|ies)|sla(y|ying|yer|in))\W?\b",
     flags=re.IGNORECASE)
