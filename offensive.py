e4ff2a7ad84ea3431ac39c142df39b4481f8ebbb
Molly White <molly.white5@gmail.com>
2015-04-25 03:11:50 -0400

    + Jenner

--- offensive.py
+++ offensive.py
@@ -10,7 +10,8 @@ offensive = re.compile(
     r"al[- ]?Qaeda|blasts?|violen(t|ce)|lethal|cancer(ous)?|stab(bed|bing|ber)?s?|casualt(y|ies)|"
     r"sla(y|ying|yer|in)|drown(s|ing|ed|ings)?|bod(y|ies)|kidnap(s|ped|per|pers|ping|pings)?|"
     r"rampage|beat(ings?|en)|terminal(ly)?|abduct(s|ed|ion)?s?|missing|behead(s|ed|ings?)?|"
-    r"homicid(e|es|al)|burn(s|ed|ing)? alive|decapitated?s?|jihadi?s?t?|hang(ed|ing|s)?)\W?\b",
+    r"homicid(e|es|al)|burn(s|ed|ing)? alive|decapitated?s?|jihadi?s?t?|hang(ed|ing|s)?|"
+    r"jenner)\W?\b",
     flags=re.IGNORECASE)
 
 
